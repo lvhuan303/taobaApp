d09133321ee80054c1196f8069ca3232455ac227
lvhuan <17741393550@163.com>
2021-12-26 23:22:18 +0800

    zx-link

---
+++ serve/vue_component/src/Steps-test.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <steps :count="count" :active="active"></steps>
+    <button @click="next">下一步</button>
+  </div>
+</template>
+
+<script>
+import Steps from "./Steps";
+export default {
+  name: "Steps-test.vue",
+  components:{
+    Steps
+  },
+  data() {
+    return {
+      count:10,
+      active:0,
+    }
+  },
+  methods: {
+    next() {
+      this.active++
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
